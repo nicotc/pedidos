302d844cd391e0b8f3fc20be6e220bb7a813676a
Nicolas Testagrossa <nicotestagrossa@gmail.com>
2021-12-03 18:54:20 -0400

    pdf

--- resources/views/pedidos/pdf.blade.php
+++ resources/views/pedidos/pdf.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
 
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-
     <link rel="stylesheet" href="{{ URL::to('/').'/bootstrap.min.css' }}" >
     <link rel="stylesheet" href="{{ URL::to('/').'/pdf.css' }}" >
  <style>
